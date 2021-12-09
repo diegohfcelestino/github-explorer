70f490a99813f20a888e56a3ce67c9f2bf2399f2
Paulo Martins <paulo@occupop.com>
2021-12-09 20:51:02 -0300

    added config files

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+
+import { NavBar } from "./Navbar/NavBar";
+import Profile from "./Profile/Profile";
+import RepositoryList from "./RepositoryList/RepositoryList";
+
+export {
+	NavBar,
+	Profile,
+	RepositoryList
+}
